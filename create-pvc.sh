31bce45ce894ba415fa164425b791de76cd1f787
nationminu <nationminu@users.noreply.github.com>
2019-07-26 16:02:05 +0900

    Create create-pvc.sh

---
+++ create-pvc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: my-pvc
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi" | oc create -f -
