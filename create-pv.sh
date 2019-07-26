578de3ac56f8a7e64b7e80c802eb7110164d8eb5
nationminu <nationminu@users.noreply.github.com>
2019-07-26 15:58:12 +0900

    Create create-pv.sh

---
+++ create-pv.sh
@@ -0,0 +1,27 @@
+#/usr/bin/env/bash
+
+NFS_SERVER=nfs-server.default.svc
+
+for volume in pv{1..50} ; do
+
+echo "{
+  \"apiVersion\": \"v1\",
+  \"kind\": \"PersistentVolume\",
+  \"metadata\": {
+    \"name\": \"${volume}-volume\"
+  },
+  \"spec\": {
+    \"capacity\": {
+        \"storage\": \"1Gi\"
+    },
+    \"accessModes\": [ \"ReadWriteOnce\" ],
+    \"nfs\": {
+        \"path\": \"/srv/nfs/user-vols/${volume}\",
+        \"server\": \"${NFS_SERVER}\"
+    },
+    \"persistentVolumeReclaimPolicy\": \"Recycle\"
+  }
+}" | oc create -f - ;
+
+done;
+
